20ed1c2f39dc3d5845ca8983c53bc06043daea47
Erkan Ipek <eip47144@stud.hs-furtwangen.de>
2026-05-22 01:36:17 +0200

    added premature pattern matching

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <fnmatch.h>
 
 #define MAX_RULES 64
 
@@ -9,6 +12,8 @@ struct Rules{
 };
 
 int main(){
+    int dry_run_flag = 1; // 1 = just display, 0 = move file
+
     struct Rules rules[MAX_RULES];
     int rule_count = 0;
     // open txt file
@@ -23,12 +28,14 @@ int main(){
         if (strncmp(buffer, "pattern", strlen("pattern")) == 0) {
             char *value = strchr(buffer, '=') + 2; //+2 to skip '='
             strncpy(rules[rule_count - 1].pattern, value, 255);
+            rules[rule_count - 1].pattern[strcspn(rules[rule_count - 1].pattern, "\n")] = '\0';
         }
             
 
         if (strncmp(buffer, "destination", strlen("destination")) == 0) {
             char *value = strchr(buffer, '=') + 2; //+2 to skip '='
             strncpy(rules[rule_count - 1].dest_dir, value, 255);
+            rules[rule_count - 1].dest_dir[strcspn(rules[rule_count - 1].dest_dir, "\n")] = '\0';
         }
     }
 
@@ -38,6 +45,38 @@ int main(){
     for (int i = 0; i < rule_count; i++) {
         printf("Regel %d: pattern=%s dest=%s\n", i+1, rules[i].pattern, rules[i].dest_dir);
     }
+
+    char *home_dir = getenv("HOME");
+
+    char downloads_path [512];
+
+    snprintf(downloads_path, sizeof(downloads_path), "%s/Downloads", home_dir);
+
+    DIR *dir = opendir(downloads_path);
+
+    if (dir == NULL) {
+        printf("Error opening directory...");
+        return 1;
+    }
+
+    struct dirent *entry;
+
+    while((entry = readdir(dir)) != NULL){
+        //printf("%s\n", entry->d_name);
+
+        for (int i = 0; i < rule_count; i++) {
+            if (fnmatch(rules[i].pattern, entry->d_name, 0) == 0) {
+                //printf("Match: %s -> %s\n", entry->d_name, rules[i].dest_dir);
+                if(dry_run_flag) {
+                    printf("[DRY-RUN] Würde verschieben %s -> %s\n", entry->d_name, rules[i].dest_dir);
+                } else {
+
+                }
+            }
+        }
+    }
+
+    closedir(dir);
 
     return 0;
 }
